88235bc5341de6f19e1d8bbe7c118ff7e32bca53
simo-sw <simonecapelli13@gmail.com>
2023-08-25 17:57:43 +0200

    Homepage - UsersBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import BookMeaningSection from '@/components/words/BookMeaningSection';
 import InputWordSection from '@/components/words/InputWordSection';
+import { clerkClient } from '@clerk/nextjs';
 
-const Home = () => {
+const Home = async () => {
+  const totalUsers = await clerkClient.users.getCount();
   return (
-    <section className="w-full flex-center flex-col pt-12">
+    <section className="w-full flex-center flex-col pt-14 ">
+      <br />
+      <div className="gap-4 w-full flex flex-row justify-between items-center">
+        <Image
+          src="/assets/homepage/users.png"
+          alt="users"
+          width={25}
+          height={25}
+        />
+        <div className="w-full h-6 border-2 rounded-[36px] border-black bg-white">
+          <div
+            className="h-full bg-[#8E00FD] rounded-[36px]"
+            style={{
+              width: (totalUsers * 100) / 500 + '%',
+            }}
+          ></div>
+        </div>
+        {totalUsers}/500
+      </div>
+
       <Image
         className="pt-16"
         src="/assets/images/word_big-letter_colored.png"
@@ -13,10 +34,6 @@ const Home = () => {
         width={117}
         height={117}
       />
-
-      <div className="text-medium flex flex-row gap-1 pt-10">
-        <p>Initial Page</p>
-      </div>
 
       <br />
       <br />
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,19 +1,32 @@
+'use client';
+
 import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 const Nav = () => {
+  const router = useRouter();
   return (
-    <nav className="fixed top-0 z-30 flex justify-between items-center w-full bg-slate-50 pt-2 px-6 pb-2">
-      <SignedIn>
+    <nav className="fixed top-0 z-30 flex justify-between items-center w-full bg-slate-50 pt-2 px-5 pb-2">
+      {/*  <SignedOut>
         <Image
-          src="/assets/icons/doubts-button.png"
+          src="/assets/nav/code.png"
           alt="Questions"
           width={30}
           height={30}
         />
-      </SignedIn>
+      </SignedOut> */}
 
-      <div className="header-title">
+      {/* <SignedIn>
+        <Image
+          src="/assets/nav/doubts-button.png"
+          alt="Questions"
+          width={30}
+          height={30}
+        />
+      </SignedIn> */}
+
+      <div className="header-title" onClick={() => router.push('/')}>
         <p className="text-[#268C41]">Words</p>
         <p className="px-2">&</p>
         <p className="text-[#0085FF]">Quotes</p>
@@ -25,8 +38,14 @@ const Nav = () => {
 
       <SignedOut>
         <SignInButton>
-          <button className="btn-save flex w-[80px] h-[37px] justify-center items-center rounded-[5px] border-solid border-[rgba(0,0,0,0.31) p-1 text-white font-medium">
-            Sign in
+          <button className="btn-save flex p-1 px-2 gap-1 justify-center items-center rounded-[35px] border-solid border-[rgba(0,0,0,0.31) text-white font-medium">
+            <p className="text-[14px]">Join us</p>
+            <Image
+              src="/assets/nav/stars.png"
+              alt="stars"
+              width={18}
+              height={18}
+            />
           </button>
         </SignInButton>
       </SignedOut>
