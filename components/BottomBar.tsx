4dc53693e1b6f0b9e452001335d54dfe8b058710
simo-sw <simonecapelli13@gmail.com>
2023-08-26 12:09:46 +0200

    Bottombar improved on different routes

--- components/BottomBar.tsx
+++ components/BottomBar.tsx
@@ -1,37 +1,114 @@
 'use client';
 
+import { SignedIn, SignedOut } from '@clerk/nextjs';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 const BottomBar = () => {
   const router = useRouter();
-  return (
-    <section className="bottombar">
-      <div className="bottombar_container">
-        <Image
+  const pathname = usePathname();
+
+  {
+    /* <Image
           src="/assets/icons/feedback.png"
           alt="feedback"
           width={32}
           height={32}
-        />
-        <Image
-          onClick={() => {
-            router.push('/words');
-          }}
-          src="/assets/images/word_medium-letter_white.png"
-          alt="Word_letter white"
-          width={35}
-          height={35}
-        />
-        <Image
-          onClick={() => {
-            router.push('/words/storage');
-          }}
-          src="/assets/icons/storage.png"
-          alt="Storage"
-          width={32}
-          height={32}
-        />
+        /> */
+  }
+
+  return (
+    <section className="bottombar">
+      <div className="bottombar_container">
+        {pathname === '/' && (
+          <>
+            <div className="outfit white-circle">menu</div>
+            <SignedOut>
+              <Image
+                src="/assets/bottombar/home.png"
+                alt="home"
+                width={32}
+                height={32}
+              />
+            </SignedOut>
+          </>
+        )}
+        <SignedIn>
+          {pathname === '/' && (
+            <>
+              <Image
+                onClick={() => {
+                  router.push('/words');
+                }}
+                src="/assets/images/word_medium-letter_white.png"
+                alt="Word_letter white"
+                width={40}
+                height={40}
+              />
+
+              <div
+                className="white-circle"
+                onClick={() => {
+                  router.push('/words/storage');
+                }}
+              >
+                <Image
+                  src="/assets/bottombar/storage.png"
+                  alt="Storage"
+                  width={30}
+                  height={30}
+                />
+              </div>
+            </>
+          )}
+          {(pathname === '/words' || pathname === '/words/storage') && (
+            <>
+              <div className="white-circle">
+                <Image
+                  src="/assets/bottombar/tutorial.png"
+                  alt="Tutorial"
+                  width={30}
+                  height={30}
+                />
+              </div>
+              <Image
+                onClick={() => {
+                  router.push('/');
+                }}
+                src="/assets/bottombar/home.png"
+                alt="home"
+                width={32}
+                height={32}
+              />
+            </>
+          )}
+          {pathname === '/words' && (
+            <div
+              className="white-circle"
+              onClick={() => {
+                router.push('/words/storage');
+              }}
+            >
+              <Image
+                src="/assets/bottombar/storage.png"
+                alt="Storage"
+                width={30}
+                height={30}
+              />
+            </div>
+          )}
+          {pathname === '/words/storage' && (
+            <Image
+              onClick={() => {
+                router.push('/words');
+              }}
+              src="/assets/images/word_medium-letter_white.png"
+              alt="Word_letter white"
+              width={40}
+              height={40}
+            />
+          )}
+        </SignedIn>
       </div>
     </section>
   );
